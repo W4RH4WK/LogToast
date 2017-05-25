c40ae55420e4cf398f81c0a5f52f9c7499b63c3d
Alex Hirsch <w4rh4wk@bluephoenix.at>
2017-05-25 16:54:26 +0200

    Wrap initialization in its own function

--- logger.c
+++ logger.c
@@ -5,14 +5,49 @@
 #include <stdlib.h>
 #include <time.h>
 
+static bool logger_initialized = false;
+
 static FILE* log_file;
-static int log_level = -1;
-static int log_location = -1;
-static int should_colorize = -1;
+static int log_level;
+static int log_location;
+static int should_colorize;
 
 static void close_log_file(void)
 {
 	fclose(log_file);
+}
+
+static void logger_init(void)
+{
+	/* determine log_file */
+	const char* f = getenv("LOG_FILE");
+	if (f && strcmp(f, "-") != 0) {
+		log_file = fopen(f, "a");
+		if (!log_file) {
+			perror("Logger");
+			exit(1);
+		}
+		atexit(close_log_file);
+	} else if (f) {
+		log_file = stdout;
+	} else {
+		log_file = stderr;
+	}
+
+	/* determine log_level */
+	const char* l = getenv("LOG_LEVEL");
+	log_level = l ? atoi(l) : LOG_LEVEL_DEFAULT;
+
+	/* determine log_location */
+	log_location = getenv("LOG_LOCATION") != NULL;
+
+	/* determine should_colorize */
+	const char* t = getenv("TERM");
+	should_colorize =  getenv("DISABLE_LOG_COLOR") == NULL
+					&& (log_file == stderr || log_file == stdout)
+					&& (t && strcmp(t, "dumb"));
+
+	logger_initialized = true;
 }
 
 void logger_log(int level, const char* color, bool terminate, const char* file,
@@ -21,40 +56,8 @@ void logger_log(int level, const char* color, bool terminate, const char* file,
 	va_list args;
 	va_start(args, fmt);
 
-	/* determine log_file */
-	if (log_file == NULL) {
-		const char* f = getenv("LOG_FILE");
-		if (f && strcmp(f, "-") != 0) {
-			log_file = fopen(f, "a");
-			if (!log_file) {
-				perror("Logger");
-				exit(1);
-			}
-			atexit(close_log_file);
-		} else if (f) {
-			log_file = stdout;
-		} else {
-			log_file = stderr;
-		}
-	}
-
-	/* determine log_level */
-	if (log_level == -1) {
-		const char* l = getenv("LOG_LEVEL");
-		log_level = l ? atoi(l) : LOG_LEVEL_DEFAULT;
-	}
-
-	/* determine log_location */
-	if (log_location == -1) {
-		log_location = getenv("LOG_LOCATION") != NULL;
-	}
-
-	/* determine should_colorize */
-	if (should_colorize == -1) {
-		const char* t = getenv("TERM");
-		should_colorize =  getenv("DISABLE_LOG_COLOR") == NULL
-		                && (log_file == stderr || log_file == stdout)
-		                && (t && strcmp(t, "dumb"));
+	if (!logger_initialized) {
+		logger_init();
 	}
 
 	if (log_level < level) {
